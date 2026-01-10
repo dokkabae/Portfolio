f5e82732a7c07c23c28475b6df4e8625f96de6a3
Ian Du Toit <27049744@sun.ac.za>
2026-01-11 00:57:44 +0200

    updating site for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Experience from "./pages/Experience";
@@ -10,7 +10,7 @@ function App() {
   
     return(
 
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
@@ -19,7 +19,7 @@ function App() {
                 <Route path="blog" element={<Blog />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
     );
 }
