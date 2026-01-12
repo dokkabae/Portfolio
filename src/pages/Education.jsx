2a9a6c1f8684ef1e4b2f87106b75f084c5b2a27b
Ian Du Toit <27049744@sun.ac.za>
2026-01-12 14:14:01 +0200

    small style changes on education page, renamed header

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -35,39 +35,39 @@ export default function Education() {
           <h3 className="headings">First year</h3>
 
           <ul className="education_list">
-            <li>Math 114 - 58 (Intro to Calculus)</li>
-            <li>Applied Math 114 - 60 (Intro to Applied Mathematics)</li>
-            <li>Probability Theory and Statistics 114 - 51 (Probability Theory)</li>
-            <li>Computer Science 114 - 62 (Intro to Programming)</li>
-            <li>Science in Context 178 - 72 (Research Theory)</li>
-            <li>Data Science 114 - 56 (Data Analytics)</li>
-            <li>Operations Research 114 - 57 (Logistics Optimization Algorithms)</li>
-            <li>Math 144 - 56 (Calculus I)</li>
-            <li>Applied Math 144 - 50 (Linear Algebra I)</li>
-            <li>Computer Science 144 - 60 (Object Oriented Programming)</li>
-            <li>Operations Research 144 - 54 (Intro to Linear Programming)</li>
+            <li>Math 114 - 58 <br></br> (Intro to Calculus)</li>
+            <li>Applied Math 114 - 60 <br></br> (Intro to Applied Mathematics)</li>
+            <li>Probability Theory and Stats 114 - 51 <br></br> (Probability Theory)</li>
+            <li>Computer Science 114 - 62 <br></br> (Intro to Programming)</li>
+            <li>Science in Context 178 - 72 <br></br> (Research Theory)</li>
+            <li>Data Science 114 - 56 <br></br> (Data Analytics)</li>
+            <li>Operations Research 114 - 57 <br></br> (Logistics Optimization Algorithms)</li>
+            <li>Math 144 - 56 <br></br> (Calculus I)</li>
+            <li>Applied Math 144 - 50 <br></br> (Linear Algebra I)</li>
+            <li>Computer Science 144 - 60 <br></br> (Object Oriented Programming)</li>
+            <li>Operations Research 144 - 54 <br></br> (Intro to Linear Programming)</li>
           </ul>
 
           <h3 className="headings">Second year</h3>
 
           <ul className="education_list">
-            <li>Math 214 - 58 (Linear Algebra II and Calculus II)</li>
-            <li>Applied Math 214 - 67 (Differential Equations)</li>
-            <li>Operations Research 214 - 58 (Linear Programming)</li>
-            <li>Computer Science 214 - 62 (Data Structures and Algorithms)</li>
-            <li>Math 244 - 50 (Linear Algebra III and Calculus III)</li>
-            <li>Computer Science 244 - 62 (Low Level Computer Architecture)</li>
+            <li>Math 214 - 58 <br></br> (Linear Algebra II and Calculus II)</li>
+            <li>Applied Math 214 - 67 <br></br> (Differential Equations)</li>
+            <li>Operations Research 214 - 58 <br></br> (Linear Programming)</li>
+            <li>Computer Science 214 - 62 <br></br> (Data Structures and Algorithms)</li>
+            <li>Math 244 - 50 <br></br> (Linear Algebra III and Calculus III)</li>
+            <li>Computer Science 244 - 62 <br></br> (Low Level Computer Architecture)</li>
           </ul>
 
           <h3 className="headings">Third year</h3>
 
           <ul className="education_list">
-            <li>Computer science 314 - 60 (Concurrent Programming)</li>
-            <li>Computer science 313 - 71 (Advanced Computer Networks)</li>
-            <li>Operations Research 314 - 57 (Dynamic Programming) </li>
-            <li>Operations Research 322 - 50 (Queueing Theory & Simulation Modelling)</li>
-            <li>Computer science 343 - 81 (Website Development)</li>
-            <li>Computer science 344 - 68 (Software Engineering)</li>
+            <li>Computer science 314 - 60 <br></br> (Concurrent Programming)</li>
+            <li>Computer science 313 - 71 <br></br> (Advanced Computer Networks)</li>
+            <li>Operations Research 314 - 57 <br></br> (Dynamic Programming) </li>
+            <li>Operations Research 322 - 50 <br></br> (Queueing Theory & Simulation Modelling)</li>
+            <li>Computer science 343 - 81 <br></br> (Website Development)</li>
+            <li>Computer science 344 - 68 <br></br> (Software Engineering)</li>
           </ul>
         </div>
       </section>
