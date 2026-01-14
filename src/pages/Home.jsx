70d1e8ea3ab70670b6991b5c2318b1a2d47250c0
Ian Du Toit <27049744@sun.ac.za>
2026-01-14 22:26:26 +0200

    fixed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,13 @@ export default function Home() {
     <div className="home">
       <section className="edit-card">
         <div className="greeting">
-          <h1>Hi, welcome to my portfolio</h1>
-          <h3>My name is Ian du Toit</h3>
-          <h4>I'm a final year computer science undergraduate</h4>
-          <h4>I built this website using React and Vite</h4>
-          <h4>Feel free to have a look around and e-mail me</h4>
-          <h4>at ian@dutoit.ws for any inquiries</h4>
+          <h1 id = "heading">Hi, welcome to my portfolio</h1>
+          <div id = "body">
+            My name is Ian du Toit<br></br><br></br>
+            I'm a final year computer science undergraduate<br></br><br></br>
+            Feel free to have a look around and e-mail me<br></br><br></br>
+            at ian@dutoit.ws for any inquiries<br></br><br></br>
+          </div>
         </div>
       </section>
     </div>
